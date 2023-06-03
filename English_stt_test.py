c1cf38c109d865cc786c4c5da0e925ff63d1b741
jason <jsluo0413@gmail.com>
2023-06-02 21:41:26 -0400

    Fixed the typo

--- English_stt_test.py
+++ English_stt_test.py
@@ -8,7 +8,7 @@ model.to("cuda")
 print("Start Transcribe")
 start_time = time.time()
 convert_to_wav("./test_audio/test_5.m4a")
-result = model.transcribe("./test_audio/output.wav")
+result = model.transcribe("./test_audio/test_5.wav")
 end_time = time.time()
 
 elapsed_time = end_time - start_time
--- chinese_stt_test.py
+++ chinese_stt_test.py
@@ -3,7 +3,7 @@ from funasr_onnx import Paraformer
 model_dir = "./paraformer-large"
 
 model = Paraformer(model_dir,batch_size =1, quantize=True)
-wav_path = ['./test_audio/outpu.wav']
+wav_path = ['./test_audio/output.wav']
 result = model(wav_path)
 print(result)
 
